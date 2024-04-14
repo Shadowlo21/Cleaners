7864ac798a1ef954f5170ff3b2c895916ba78086
Mr.Shadow <97762724+Shadowlo21@users.noreply.github.com>
2024-04-14 11:30:44 +0200

    Update COD.h

--- COD.h
+++ COD.h
@@ -2,83 +2,68 @@
 #include "smth.h"
 
 void CODClean() {
-
-
-
-		killer();
-		
-        system(skCrypt("rmdir /s /q \"%localappdata%\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%localappdata%\\Blizzard Entertainment\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%appdata%\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%programdata%\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%programdata%\\Blizzard Entertainment\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%programdata%\\Activision\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%USERPROFILE%\\Documents\\Call Of Duty Black Ops Cold War\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%USERPROFILE%\\Documents\\Call of Duty Modern Warfare\"").decrypt());
-
-        // Delete specific files related to Call of Duty Modern Warfare
-        system(skCrypt("del \"%codmw%\\Data\\data\\shmem\" /s /q").decrypt());
-        system(skCrypt("del \"%codmw%\\main\\recipes\\cmr_hist\" /s /q").decrypt());
-
-        // Delete additional directories
-        system(skCrypt("rmdir /s /q \"%localappdata%\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%localappdata%\\Blizzard Entertainment\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%appdata%\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%programdata%\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%programdata%\\Blizzard Entertainment\"").decrypt());
-        system(skCrypt("rmdir /s /q \"%USERPROFILE%\\Documents\\Call of Duty Modern Warfare\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Battle.net\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\12On7\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\BlizzardBrowser\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\main\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\telescopeCache\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\xpak_cache\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\Data\\indices\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\Data\\config\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\ProgramData\\NVIDIA Corporation\\NV_Cache\"").decrypt());
-        system(skCrypt("rmdir /s /q \"c:\\ProgramData\\NVIDIA Corporation\\NV_Cache\"").decrypt());
-
-        // Delete registry entries
-        system(skCrypt("reg delete HKCU\\Software\\Blizzard Entertainment\\Battle.net /f").decrypt());
-        system(skCrypt("reg delete HKLM\\Software\\WOW6432Node\\Blizzard Entertainment /f").decrypt());
-        system(skCrypt("reg delete HKEY_CURRENT_USER\\Software\\Blizzard Entertainment\\Battle.net\\Identity /f").decrypt());
-        system(skCrypt("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\UserAssist /f").decrypt());
-        system(skCrypt("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\DirectInput /f").decrypt());
-        system(skCrypt("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\BitBucket\\Volume /f").decrypt());
-        system(skCrypt("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\MountPoints2 /f").decrypt());
-        system(skCrypt("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\ActivityDataModel /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\battlenet /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\blizzard /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.Battlenet /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.Blizzard /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.Heroes /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.SC2 /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\heroes /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\starcraft /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\battlenet /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001 /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\HardwareConfig /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\MultifunctionAdapter\\0\\DiskController\\0\\DiskPeripheral /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\BIOS /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\ComputerName /f").decrypt());
-        system(skCrypt("reg delete HKEY_LOCAL_MACHINE\\HARDWARE\\DEVICEMAP\\VIDEO /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\battlenet /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\blizzard /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.Battlenet /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.Blizzard /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.Heroes /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.SC2 /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\heroes /f").decrypt());
-        system(skCrypt("reg delete HKEY_CLASSES_ROOT\\starcraft /f").decrypt());
-
-        // Delete additional directories and temporary files
-        system(skCrypt("del C:\\ProgramData\\Battle.net /s /q").decrypt());
-        system(skCrypt("del C:\\ProgramData\\Blizzard Entertainment /s /q").decrypt());
-        system(skCrypt("del C:\\Users\\%USERNAME%\\AppData\\Local\\Temp /s /q").decrypt());
-        system(skCrypt("del C:\\Windows\\Temp /s /q").decrypt());
-
-
-		MessageBoxA(0, skCrypt("Clean Done").decrypt(), skCrypt("Shadow").decrypt(), 0);
-
+	killer();
+        system("rmdir /s /q \"%localappdata%\\Battle.net\"");
+        system("rmdir /s /q \"%localappdata%\\Blizzard Entertainment\"");
+        system("rmdir /s /q \"%appdata%\\Battle.net\"");
+        system("rmdir /s /q \"%programdata%\\Battle.net\"");
+        system("rmdir /s /q \"%programdata%\\Blizzard Entertainment\"");
+        system("rmdir /s /q \"%programdata%\\Activision\"");
+        system("rmdir /s /q \"%USERPROFILE%\\Documents\\Call Of Duty Black Ops Cold War\"");
+        system("rmdir /s /q \"%USERPROFILE%\\Documents\\Call of Duty Modern Warfare\"");
+        system("del \"%codmw%\\Data\\data\\shmem\" /s /q");
+        system("del \"%codmw%\\main\\recipes\\cmr_hist\" /s /q");
+        system("rmdir /s /q \"%localappdata%\\Battle.net\"");
+        system("rmdir /s /q \"%localappdata%\\Blizzard Entertainment\"");
+        system("rmdir /s /q \"%appdata%\\Battle.net\"");
+        system("rmdir /s /q \"%programdata%\\Battle.net\"");
+        system("rmdir /s /q \"%programdata%\\Blizzard Entertainment\"");
+        system("rmdir /s /q \"%USERPROFILE%\\Documents\\Call of Duty Modern Warfare\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Battle.net\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\12On7\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\BlizzardBrowser\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\main\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\telescopeCache\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\xpak_cache\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\Data\\indices\"");
+        system("rmdir /s /q \"c:\\Program Files (x86)\\Call of Duty Modern Warfare\\Data\\config\"");
+        system("rmdir /s /q \"c:\\ProgramData\\NVIDIA Corporation\\NV_Cache\"");
+        system("rmdir /s /q \"c:\\ProgramData\\NVIDIA Corporation\\NV_Cache\"");
+        system("reg delete HKCU\\Software\\Blizzard Entertainment\\Battle.net /f");
+        system("reg delete HKLM\\Software\\WOW6432Node\\Blizzard Entertainment /f");
+        system("reg delete HKEY_CURRENT_USER\\Software\\Blizzard Entertainment\\Battle.net\\Identity /f");
+        system("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\UserAssist /f");
+        system("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\DirectInput /f");
+        system("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\BitBucket\\Volume /f");
+        system("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\MountPoints2 /f");
+        system("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\ActivityDataModel /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\battlenet /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\blizzard /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.Battlenet /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.Blizzard /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.Heroes /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\Blizzard.URI.SC2 /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\heroes /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\starcraft /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\battlenet /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001 /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\HardwareConfig /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\MultifunctionAdapter\\0\\DiskController\\0\\DiskPeripheral /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System\\BIOS /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\ComputerName /f");
+        system("reg delete HKEY_LOCAL_MACHINE\\HARDWARE\\DEVICEMAP\\VIDEO /f");
+        system("reg delete HKEY_CLASSES_ROOT\\battlenet /f");
+        system("reg delete HKEY_CLASSES_ROOT\\blizzard /f");
+        system("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.Battlenet /f");
+        system("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.Blizzard /f");
+        system("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.Heroes /f");
+        system("reg delete HKEY_CLASSES_ROOT\\Blizzard.URI.SC2 /f");
+        system("reg delete HKEY_CLASSES_ROOT\\heroes /f");
+        system("reg delete HKEY_CLASSES_ROOT\\starcraft /f");
+        system("del C:\\ProgramData\\Battle.net /s /q");
+        system("del C:\\ProgramData\\Blizzard Entertainment /s /q");
+        system("del C:\\Users\\%USERNAME%\\AppData\\Local\\Temp /s /q");
+        system("del C:\\Windows\\Temp /s /q");
+	MessageBoxA(0, "Clean Done", "Shadow", 0);
 }
